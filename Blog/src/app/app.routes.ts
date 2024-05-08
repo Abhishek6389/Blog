35768d26f4890531d11807109e46d4481fe00396
Abhishek6389 <159023186+Abhishek6389@users.noreply.github.com>
2024-05-08 14:51:08 +0530

    Add files via upload

--- Blog/src/app/app.routes.ts
+++ Blog/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 export const routes: Routes = [
     {
         path:'',
@@ -10,5 +11,11 @@ export const routes: Routes = [
     {
 path:'login',
 component:LoginComponent
-    }
+    ,
+},
+{
+    path:'main',
+    component:MainComponent
+}
+
 ];
--- Blog/src/app/login/login.component.ts
+++ Blog/src/app/login/login.component.ts
@@ -1,12 +1,63 @@
+// import { HttpClient} from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [FormsModule , HttpClient],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css'
+// })
+// export class LoginComponent {
+// username: string="";
+// password : string ="";
+
+// constructor(private http:HttpClient){}
+//  onSubmit(){
+//   this.http.post<any>("https://localhost:7202/api/UserRegistrations",{
+//   username :this.username,
+//   password:this.password
+//   }).subscribe((data) =>{
+//     console.log(data);
+//   })
+//  } 
+
+
+// }
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports : [FormsModule , HttpClientModule],
+  standalone:true
+
 })
 export class LoginComponent {
+  username: string = "";
+  password: string = "";
 
+  constructor(private http: HttpClient , private router:Router) {}
+
+  onSubmit() {
+    this.http.post("https://localhost:7202/api/Login", {
+      username: this.username,
+      password: this.password
+    }, { responseType: 'text' }).subscribe(
+      (response: any) => {
+        this.router.navigate(['/main'])
+        console.log(response); // Log or handle the response as plain text
+      },
+      (error) => {
+        console.error("Error occurred:", error);
+      }
+    );
+   
+  }
+  
 }
+
